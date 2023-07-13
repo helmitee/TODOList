6dddf635e03f3fcd54fb7191242b7b678303fe70
Helmi Tormilainen <130982018+helmitee@users.noreply.github.com>
2023-07-13 11:53:04 +0300

    Update TodoItem.jsx

--- TodoItem.jsx
+++ TodoItem.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "./todo-item.css";
 
-// Tässä komponentissa majailee yksittäinen listalla oleva
-// tehtävä. Se saa proppina oman tekstinsä.
+
 export function TodoItem(props) {
-  // Voit toteuttaa kolmostehtävässä tehtävän merkkaamisen
-  // tehdyksi esimerkiksi lisäämällä tähän useState-hookin,
-  // jossa on done-tieto boolean-arvona. Toinen tapa pitää
-  // kirjaa tehdyistä tehtävistä on tehdä se jo App.js:ssä,
-  // käyttämällä listassa stringien sijaan js-objekteja joissa
-  // on teksti _ja_ tuo boolean-arvo.
 
   const markDone = () => {
     props.taskDone(props.item);
   };
-  // Alle tuotat yksittäisen listalla olevan elementin renderöinnin.
-  // Elementin tiedot tulevat komponenttiin props-muuttujassa.
-
+  
+  // yksittäisen listalla olevan elementin renderöinti
+  // elementin tiedot tulevat komponenttiin props-muuttujassa
   return (
     <div className="todo-item">
       <p
