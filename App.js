67869c53c95e8e5c3ed9cda10243f291a1378756
Helmi Tormilainen <130982018+helmitee@users.noreply.github.com>
2023-07-13 11:52:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-// Tehtävän alkuperäinen toteutus Pekka Pullin käsialaa
+
 import { every } from "lodash";
 import React, { useState } from "react";
 import "./styles.css";
 import { TodoList } from "./TodoList/TodoList";
 import { AddInput } from "./AddInput";
 
-// Voit käyttää tätä listaa ykköstehtävässä.
-// Kakkostehtävää varten voit alustaa oman listasi
-// tyhjänä, eli tämän listan ei tarvitse näkyä
-// lopullisessa palautuksessa
 
 export default function App() {
-  // Tee tähän kakkostehtävässä Reactin tilaan kytkeytyvä
-  // useState-hook, joka pitää sisällään Todo-listan elementit.
-  // Lista voi olla aluksi tyhjä ([]). Muista vaihtaa tämä uusi
-  // lista myös TodoListin proppeihin!
+  
   const [list, setList] = useState([]);
 
   function addListItem(item) {
@@ -35,19 +28,7 @@ export default function App() {
     ? "ei keskeneräisiä tehtäviä"
     : "";
 
-  // Tähän kannattaa tehdä kaikki funktiot, joilla listaa
-  // muokataan. Esim. listaan lisääminen, listasta poistaminen,
-  // listaelementin siirtäminen. Funktiot voi lähettää muille
-  // komponenteille propseina.
-
-  // Allaolevassa rakenteessa on jo <TodoList />, jolle sinun tulee
-  // antaa proppina todo-listan elementit. Mene sitten TodoList.jsx:ään
-  // ottamaan prop vastaan ja luomaan itse lista.
-
-  // Kakkostehtävässä sinun tulee vielä tuoda tähän
-  // AddInput-komponentti, joka löytyy valmiina AddInput-hakemistosta.
-  // Sillä lisätään komponentit listaan. Anna sille proppina
-  // funktio, jolla lisäät elementin tekemääsi useState-hookiin.
+  
   return (
     <div className="App">
       <h1>TO-DO</h1>
