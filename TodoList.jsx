ca71a34711c34e9199873ca538da9967fbf69eb1
Helmi Tormilainen <130982018+helmitee@users.noreply.github.com>
2023-07-13 11:53:36 +0300

    Update TodoList.jsx

--- TodoList.jsx
+++ TodoList.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { TodoItem } from "./TodoItem";
 
-// Tuo tähän elementtiin proppina todo-listasi tehtävät
-// (esim. props.items jos items oli propin nimi).
-// Tuota map-funktiolla lista tehtävistä. Jokainen
-// listan elementti on TodoItem-komponentti, joka löytyy
-// TodoItem.jsx-tiedostosta. Muista, että React-elementtejä
-// tuotetaan kirjoittamalla <TodoItem /> eikä esim. TodoItem().
+// proppina todo-listasi tehtävät
 export const TodoList = (props) => {
   const elements = props.listItems.map((item, index) => (
     <TodoItem key={index} item={item} taskDone={props.taskDone}></TodoItem>
